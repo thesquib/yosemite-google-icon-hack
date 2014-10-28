6a762290120d45f69d5ba65d337dcd0ad905ec42
thesquib <stock.pile@gmail.com>
2014-10-28 21:02:51 +1300

    Tested and running on 10.10

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ yosemite-google-drive-fix
 =========================
 
 
-This *will* (not yet) fix the Google Drive menu bar icon when using the dark theme in Yosemite. It is a reversible operation, you can just run the script again.
+This script is a quick hack to fix thhe Google Drive menu bar icon when using the dark theme in Yosemite. It is a reversible operation, you can just run the script again.
--- fix_google_drive.sh
+++ fix_google_drive.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 #Unfinished, do not run yet.
-# DRIVE_LOCATION="/Applications/Google Drive.app"
+DRIVE_LOCATION="/Applications/Google Drive.app"
 
-# if [[ ("$#" -eq 1) && ($1 =~ .*Google\ Drive\.app) ]]; then
-# 	DRIVE_LOCATION=$1
-# fi
+if [[ ("$#" -eq 1) && ($1 =~ .*Google\ Drive\.app) ]]; then
+	DRIVE_LOCATION=$1
+fi
 
-# RESOURCES=$DRIVE_LOCATION/Contents/Resources
+RESOURCES=$DRIVE_LOCATION/Contents/Resources
 
-# echo $RESOURCES
+echo $RESOURCES
 
-# if [[ ! -d $DRIVE_LOCATION ]]; then
-# 	echo "Google Drive not found!"
-# 	exit 1
-# fi
+if [[ ! -d $DRIVE_LOCATION ]]; then
+	echo "Google Drive not found!"
+	exit 1
+fi
 
-# for x in mac-*2x.png; do echo ${x%@2x.png}; mv $x ${x%@2x.png}-inverse@2x.png; done
-# for x in mac-*[0-8].png; do echo ${x%.png}; mv $x ${x%.png}-inverse.png; done
-# for x in moving_mac-*; do mv $x  `echo $x | sed -e "s/-inverse//gI"`; done
-# for x in moving_mac-*; do mv $x  `echo $x | sed -e "s/moving_//gI"`; done
+cd "$RESOURCES"
+for x in mac-*-inverse*.png; do 
+	mv "$x" "moving_$x"
+done
+
+for x in mac-*2x.png; do 
+	NEW_NAME="${x%@2x.png}-inverse@2x.png"
+	echo "$x -> $NEW_NAME"
+	mv "$x" "$NEW_NAME"
+done
+
+for x in mac-*[0-8].png; do
+	NEW_NAME="${x%.png}-inverse.png"
+	echo "$x -> $NEW_NAME"
+	mv "$x" "${x%.png}-inverse.png"
+done
+
+for x in moving_mac-*; do
+	NEW_NAME=`echo "$x" | sed -e "s/-inverse//gI" -e "s/moving_//gI"`
+	echo "$x -> $NEW_NAME"
+	mv "$x" "$NEW_NAME"
+done
